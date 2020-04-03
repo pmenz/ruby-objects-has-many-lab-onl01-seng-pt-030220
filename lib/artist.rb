f7f51f26cc30aeeb727ffffc00888873acb04d4f
Pablo Menz <p.menz.r@gmail.com>
2020-04-03 20:29:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,26 +1,32 @@
-class Artist
   
+class Artist
   attr_accessor :name
- 
+
+  @@all = []
+
   def initialize(name)
     @name = name
-    @songs = []
+    @@all << self
   end
- 
+
+  def self.all
+    @@all
+  end
+
   def add_song(song)
-    @songs << song
     song.artist = self
   end
-  def add_song_by_name
- 
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
   end
- 
-  def song_count
-   
-  end
- 
+
   def songs
-  self
+    Song.all.select {|song| song.artist == self}
   end
-  
+
+  def self.song_count
+    Song.all.count
+  end
 end
